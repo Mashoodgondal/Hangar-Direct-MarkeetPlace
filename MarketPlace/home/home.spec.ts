0e5ed1556c30cdacf1123a38f5582f7543fe9d6e
mashoodgondal <mashoodgondal6146@gmail.com>
2025-11-18 12:30:07 +0500

    start automation on hangar direct

---
+++ MarketPlace/home/home.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../../pages/homePage";
+
+
+test.describe("Home Page – Signup or Avatar Flow", () => {
+  
+test('checkt page visibulity',async({page})=>{
+  const home = new HomePage(page)
+  home.openHome
+  home.checkPageVisible()
+  home.checkButton()
+  home.checkAvator()
+  home.isSignupVisible()
+  home.isUserLogedIn()
+     const login = await home.isUserLogedIn()
+     if(login){
+      console.log("user is logedIn")
+     await expect(home.avatar()).toBeVisible();
+     }
+     else{
+     console.log("User is not logedIn")
+     }
+    await home.clickSignupAndGoToLogin()
+   
+  
+})
+
+})
+//   test("Verify avatar if logged in OR click signup if logged out", async ({ page }) => {
+  //     const home = new HomePage(page);
+
+//     // Step 1: Open main page
+//     await home.openHome();
+
+//     // Step 2: Check if user is already logged in
+//     const loggedIn = await home.isUserLoggedIn();
+
+//     if (loggedIn) {
+//       console.log("User is logged in → Avatar found.");
+//       await expect(home.avatar()).toBeVisible();
+//     } 
+//     else {
+//       console.log("User is NOT logged in → Signup button visible.");
+
+//       // Signup button should be visible
+//       await expect(home.signInButton()).toBeVisible();
+
+
+//       // Step 3: Click Signup and verify navigation
+//       await home.clickSignupAndGoToLogin();
+
+//       // Step 4: Verify landing page is login/signup page
+//       await expect(page.url()).toMatch('https://hangardirect-github-io.vercel.app/signin?ref=/');
+//     }
+//   });
+
+// });
---
+++ pages/homePage.ts
@@ -0,0 +1,41 @@
+import { Page, expect } from "@playwright/test";
+
+export class HomePage {
+  constructor(private page: Page) {}
+
+  // // Stable button locator even with SVG inside
+  signInButton = () =>
+    this.page.locator("button", { hasText: "Sign in" });
+
+  // Stable avatar locator
+  avatar = () => this.page.locator('button[aria-haspopup="menu"]');
+
+  async openHome() {
+    await this.page.goto("https://hangardirect-github-io.vercel.app/");
+  }
+  async checkPageVisible(){
+    await this.page.getByText('Your one place for hangar space')
+  }
+  async checkButton(){
+    //  this.page.locator("button", {hasText : "Sign in"});
+     this.signInButton()
+  }
+  async checkAvator(){
+    this.avatar()
+  }
+  async isUserLogedIn(){
+    return await this.avatar().isVisible().catch(() => false);
+  }
+  async isSignupVisible(){
+    return await this.signInButton().isVisible().catch(() => false);
+  }
+  async clickSignupAndGoToLogin() {
+    if (await this.isSignupVisible()) {
+      await this.signInButton().click();
+      await this.page.waitForLoadState("networkidle");
+      await expect(this.page.getByText("Welcome Back")).toBeVisible()
+      
+    }
+  }
+ 
+}
