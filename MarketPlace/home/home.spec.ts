7821463259bc5cb0d9d65af41e9c3dda347404c3
mashoodgondal <mashoodgondal6146@gmail.com>
2025-11-21 15:28:14 +0500

    bug tracking in profile

--- MarketPlace/home/home.spec.ts
+++ MarketPlace/home/home.spec.ts
@@ -8,18 +8,18 @@ test('checkt page visibulity',async({page})=>{
   const home = new HomePage(page)
   home.openHome
   home.checkPageVisible()
-  home.checkButton()
-  home.checkAvator()
-  home.isSignupVisible()
-  home.isUserLogedIn()
-     const login = await home.isUserLogedIn()
-     if(login){
-      console.log("user is logedIn")
-     await expect(home.avatar()).toBeVisible();
-     }
-     else{
-     console.log("User is not logedIn")
-     }
+  // home.checkButton()
+  // home.checkAvator()
+  // home.isSignupVisible()
+  // home.isUserLogedIn()
+    //  const login = await home.isUserLogedIn()
+    //  if(login){
+    //   console.log("user is logedIn")
+    //  await expect(home.avatar()).toBeVisible();
+    //  }
+    //  else{
+    //  console.log("User is not logedIn")
+    //  }
     await home.clickSignupAndGoToLogin()
    
   
