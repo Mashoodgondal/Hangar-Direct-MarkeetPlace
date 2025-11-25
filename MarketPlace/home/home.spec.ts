6948d37b1f38ffa41f60a8134c07b879466ea3b8
mashoodgondal <mashoodgondal6146@gmail.com>
2025-11-25 18:34:00 +0500

    previous tests

--- MarketPlace/home/home.spec.ts
+++ MarketPlace/home/home.spec.ts
@@ -6,20 +6,20 @@ test.describe("Home Page – Signup or Avatar Flow", () => {
   
 test('checkt page visibulity',async({page})=>{
   const home = new HomePage(page)
-  home.openHome
-  home.checkPageVisible()
+  // home.openHome
+  // home.checkPageVisible()
   // home.checkButton()
   // home.checkAvator()
   // home.isSignupVisible()
   // home.isUserLogedIn()
-    //  const login = await home.isUserLogedIn()
-    //  if(login){
-    //   console.log("user is logedIn")
-    //  await expect(home.avatar()).toBeVisible();
-    //  }
-    //  else{
-    //  console.log("User is not logedIn")
-    //  }
+     const login = await home.isUserLogedIn()
+     if(login){
+      console.log("user is logedIn")
+     await expect(home.avatar()).toBeVisible();
+     }
+     else{
+     console.log("User is not logedIn")
+     }
     await home.clickSignupAndGoToLogin()
    
   
