2eab14af755e020ca7cf32fbf8d34b69e7f932bb
mashoodgondal <mashoodgondal6146@gmail.com>
2025-11-28 18:15:33 +0500

    new

--- pages/login.ts
+++ pages/login.ts
@@ -101,4 +101,11 @@ export class LoginPage {
   //     await this.loginButton().click();
   //   }
   // }
-  
+  //   async login(email: string, password: string) {
+    //     await this.waitForLoginForm();
+    //     await this.emailField().fill(email);
+    //     await this.passwordField().fill(password);
+    //     await this.loginButton().click();
+    //   }
+    // }
+    
