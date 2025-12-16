36aeddec2fb1152287029bcf6baded6644694b7d
mashoodgondal <mashoodgondal6146@gmail.com>
2025-12-16 18:21:56 +0500

    forgor password automation

--- pages/signup.ts
+++ pages/signup.ts
@@ -7,7 +7,9 @@ export class SignupPage {
   // async goto() {
   //   await this.page.goto("https://hangardirect-github-io.vercel.app/signup");
   // }
- 
+  // async goto() {
+    //   await this.page.goto("https://hangardirect-github-io.vercel.app/signup");
+    // }
 
 
 
