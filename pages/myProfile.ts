f26b659998c369b73917470038cd1cf8af206c3c
mashoodgondal <mashoodgondal6146@gmail.com>
2025-12-22 18:57:02 +0500

    add page for profile

--- pages/myProfile.ts
+++ pages/myProfile.ts
@@ -27,3 +27,14 @@ import { Page, expect } from "@playwright/test";
 //     await expect(this.header()).toBeVisible();
 //   }
 // }
+import { Page, expect } from "@playwright/test";
+
+// export class MyProfilePage {
+//   constructor(private page: Page) {}
+
+//   header = () => this.page.getByRole("heading", { name: "My Profile" });
+
+//   async verifyVisible() {
+//     await expect(this.header()).toBeVisible();
+//   }
+// }
