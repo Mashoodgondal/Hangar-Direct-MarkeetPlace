4c0129ae0b96a34f63158a038dbbdc632f80c108
mashoodgondal <mashoodgondal6146@gmail.com>
2026-01-02 20:46:23 +0500

    Add some action

--- pages/login.ts
+++ pages/login.ts
@@ -39,20 +39,6 @@
 //     await this.loginButton().click();
 //   }
 
-//   // ACTION
-//   async login(email: string, password: string) {
-//     await this.emailField().fill(email);
-//     await this.passwordField().fill(password);
-//     await this.loginButton().click();
-//   }
-
-//   // ACTION
-//   async login(email: string, password: string) {
-//     await this.emailField().fill(email);
-//     await this.passwordField().fill(password);
-//     await this.loginButton().click();
-//   }
-
 //   async waitForLoginForm() {
 //     await this.emailField().waitFor({ state: "visible" });
 //     await this.passwordField().waitFor({ state: "visible" });
