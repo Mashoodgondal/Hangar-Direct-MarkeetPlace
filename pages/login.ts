8a4ddba1edb9a3757fde7100228426b42f35a65a
mashoodgondal <mashoodgondal6146@gmail.com>
2026-01-06 22:18:36 +0500

    move towards api

--- pages/login.ts
+++ pages/login.ts
@@ -81,6 +81,21 @@
 
 //   }
 
+//   async waitForLoginForm() {
+//     await this.emailField().waitFor({ state: "visible" });
+//     await this.passwordField().waitFor({ state: "visible" });
+//   }
+
+//   async clickForgotPassword() {
+//     await this.page.getByText('Forgot Password?').click()
+//   }
+//   async gotoSinupLink() {
+
+//     await this.page.getByRole("link", { name: "Sign up" }).click();
+//     await this.page.waitForURL("https://hangardirect-github-io.vercel.app/signup");
+
+//   }
+
 
 
 
