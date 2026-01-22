9d4be8e31d279d7a7aa46a251ddd9009cdd3dc95
mashoodgondal <mashoodgondal6146@gmail.com>
2026-01-22 23:01:02 +0500

    a test for some links

--- pages/login.ts
+++ pages/login.ts
@@ -57,6 +57,16 @@
 
 
 
+//   async clickForgotPassword() {
+//     await this.page.getByText('Forgot Password?').click()
+//   }
+//   async gotoSinupLink() {
+
+//     await this.page.getByRole("link", { name: "Sign up" }).click();
+//     await this.page.waitForURL("https://hangardirect-github-io.vercel.app/signup");
+
+//   }
+
 
 //   async clickForgotPassword() {
 //     await this.page.getByText('Forgot Password?').click()
