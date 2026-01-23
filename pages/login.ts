f8f2bafb64c05488c03e97b0788345786157cb7a
mashoodgondal <mashoodgondal6146@gmail.com>
2026-01-23 21:50:29 +0500

    credentials toast

--- pages/login.ts
+++ pages/login.ts
@@ -57,29 +57,6 @@
 
 
 
-//   async clickForgotPassword() {
-//     await this.page.getByText('Forgot Password?').click()
-//   }
-//   async gotoSinupLink() {
-
-//     await this.page.getByRole("link", { name: "Sign up" }).click();
-//     await this.page.waitForURL("https://hangardirect-github-io.vercel.app/signup");
-
-//   }
-
-
-//   async clickForgotPassword() {
-//     await this.page.getByText('Forgot Password?').click()
-//   }
-//   async gotoSinupLink() {
-
-//     await this.page.getByRole("link", { name: "Sign up" }).click();
-//     await this.page.waitForURL("https://hangardirect-github-io.vercel.app/signup");
-
-//   }
-
-
-
 
 
 
